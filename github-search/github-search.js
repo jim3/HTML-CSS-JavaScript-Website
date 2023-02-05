2fe144317d15bb6e06b78d43f05ae103de076fad
jim3 <j3.casey@protonmail.com>
2023-02-05 17:23:01 -0500

    Update github-search.js

--- github-search/github-search.js
+++ github-search/github-search.js
@@ -3,25 +3,28 @@ const form = document.querySelector("#repos-form");
 const usernameInput = document.querySelector("#github-username");
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const username = usernameInput.value;
-  githubSearch(username);
+    event.preventDefault();
+    const username = usernameInput.value;
+    githubSearch(username);
 });
 
 const githubSearch = (username) => {
-  fetch(`${baseURL}/users/${username}/repos`)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-      const repoList = document.querySelector("#repos-list");
-      data.forEach((repo) => {
-        repoList.insertAdjacentHTML(
-          "beforeend",
-          `<li><a href="${repo.html_url}" target="_blank">
-        <h2>${repo.full_name}</h2><p>${repo.description}</p></a></li>`
-        );
-      });
-    });
+    fetch(`${baseURL}/users/${username}/repos`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            const repoList = document.querySelector("#repos-list");
+            
+            data.forEach((repo) => {
+                repoList.insertAdjacentHTML(
+                    "beforeend",
+                    `<li><a href="${repo.html_url}" target="_blank">
+                        <h2>${repo.full_name}</h2>
+                        <p>${repo.description}</p>
+                    </a></li>`
+                );
+            });
+        });
 };
